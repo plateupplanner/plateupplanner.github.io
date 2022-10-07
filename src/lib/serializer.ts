8701a85e3c6a0f79111e9a7126f2f2cc961a9e5f
cookies-xor-cream <aryageza@gmail.com>
2022-10-08 00:22:20 +0800

    Indentation

--- src/lib/serializer.ts
+++ src/lib/serializer.ts
@@ -4,153 +4,153 @@ import { Layout } from "../Layout";
 import { Utils } from "./utils";
 
 export class Serializer {
-    private static characterMap = new Map([
-       [0,  "0"],
-       [1,  "1"],
-       [2,  "2"],
-       [3,  "3"],
-       [4,  "4"],
-       [5,  "5"],
-       [6,  "6"],
-       [7,  "7"],
-       [8,  "8"],
-       [9,  "9"],
-       [10, "a"],
-       [11, "b"],
-       [12, "c"],
-       [13, "d"],
-       [14, "e"],
-       [15, "f"],
-    ]);
+  private static characterMap = new Map([
+    [0,  "0"],
+    [1,  "1"],
+    [2,  "2"],
+    [3,  "3"],
+    [4,  "4"],
+    [5,  "5"],
+    [6,  "6"],
+    [7,  "7"],
+    [8,  "8"],
+    [9,  "9"],
+    [10, "a"],
+    [11, "b"],
+    [12, "c"],
+    [13, "d"],
+    [14, "e"],
+    [15, "f"],
+  ]);
 
-    private static characterUnMap = new Map([
-        ["0", 0],
-        ["1", 1],
-        ["2", 2],
-        ["3", 3],
-        ["4", 4],
-        ["5", 5],
-        ["6", 6],
-        ["7", 7],
-        ["8", 8],
-        ["9", 9],
-        ["a", 10],
-        ["b", 11],
-        ["c", 12],
-        ["d", 13],
-        ["e", 14],
-        ["f", 15],
-     ]);
+  private static characterUnMap = new Map([
+    ["0", 0],
+    ["1", 1],
+    ["2", 2],
+    ["3", 3],
+    ["4", 4],
+    ["5", 5],
+    ["6", 6],
+    ["7", 7],
+    ["8", 8],
+    ["9", 9],
+    ["a", 10],
+    ["b", 11],
+    ["c", 12],
+    ["d", 13],
+    ["e", 14],
+    ["f", 15],
+  ]);
 
-     private static wallEncodeMap = new Map([
-        ["line-empty",   0b11],
-        ["line-wall",    0b01],
-        ["line-half",    0b10],
-    ]);
+  private static wallEncodeMap = new Map([
+    ["line-empty",   0b11],
+    ["line-wall",    0b01],
+    ["line-half",    0b10],
+  ]);
 
-    private static wallDecodeMap = new Map([
-        [0b11, "0"],
-        [0b01, "w"],
-        [0b10, "h"],
-    ]);
+  private static wallDecodeMap = new Map([
+    [0b11, "0"],
+    [0b01, "w"],
+    [0b10, "h"],
+  ]);
 
-    private static serializeRowWalls(elements: (SquareType | WallType)[]) {
-        const binaryList = elements.map((element) => {
-            if ('className' in element) {
-                return Serializer.wallEncodeMap.get(element.className) as number;
-            } else {
-                return 0b00;
-            }
-        }).filter((x) => !!x);
-
-        const hexString = Utils.chunk(binaryList, 2)
-            .map((arr: any) => (arr[0] << 0) | (arr[1] << 2))
-            .map((num: number) => Serializer.characterMap.get(num))
-            .join('');
-
-        return hexString;
-    }
-
-    static serializeWalls(layout: Layout) {
-        const walls = layout.layout
-            .map((row, i) => {
-                return row.filter((_, j) => i % 2 === 0 || j % 2 === 0);
-            })
-            .flat();
-        return Serializer.serializeRowWalls(walls);
-    }
-
-    static *deserializeWalls(wallEncoding: string): Generator<string, null, null> {
-        const walls = wallEncoding
-            .split('')
-            .map((char) => Serializer.characterUnMap.get(char) as number)
-            .map((num) => {
-                return [
-                (num & 0b0011) >> 0,
-                (num & 0b1100) >> 2,
-            ]})
-            .flat()
-            .map((wallCode) => Serializer.wallDecodeMap.get(wallCode) as string);
-
-        for (const wall of walls) {
-            yield wall;
-        }
-
-        return null;
-    }
-
-    static encodeLayoutString(layout: Layout) {
-        let layoutString = `v1 ${layout.height}x${layout.width} `;
-        for (let i = 0; i < layout.height * 2 - 1; i++) {
-          for (let j = 0; j < layout.width * 2 - 1; j++) {
-            const element = layout.layout[i][j];
-            const notCornerWall = i % 2 === 0 || j % 2 === 0;
-            const isWall = 'className' in element;
-            if (notCornerWall && !isWall) {   // Skip corner walls
-              layoutString += element.getStrRepr();
-            }
-          }
-        }
-
-        layoutString += ` ${Serializer.serializeWalls(layout)}`;
-        return encodeURI(LZString.compressToEncodedURIComponent(layoutString));
-    }
-
-    static decodeLayoutString(compressedLayoutString: string) {
-        let decompressed = LZString.decompressFromEncodedURIComponent(decodeURI(compressedLayoutString).slice(1));
-        if (decompressed === null) {
-          throw new URIError("Invalid layout string, decompression failed");
-        }
-        let [version, size, layoutString, wallString] = decompressed.split(" ");
-        if (version !== "v1") {
-          throw new URIError("Invalid layout string version");
-        }
-        let [height, width] = size.split("x").map((x) => parseInt(x));
-
-        const wallsDecoded = Serializer.deserializeWalls(wallString);
-
-        let layout = new Layout(height, width);
-        for (let i = 0; i < layout.height * 2 - 1; i++) {
-          for (let j = 0; j < layout.width * 2 - 1; j++) {
-            // Squares (2 characters + 1 for rotation)
-            if (i % 2 === 0 && j % 2 === 0) {
-              let squareStrRepr = layoutString.slice(0, 3);
-              layoutString = layoutString.slice(3);
-              layout.setElement(i, j, SquareType.fromStrRepr(squareStrRepr));
-            // Walls (1 character)
-            } else if (i % 2 === 0 || j % 2 === 0) {
-              let wallStrRepr = wallsDecoded.next().value;
-              if (!!wallStrRepr) {
-                const wall = WallType.fromStrRepr(wallStrRepr)
-                layout.setElement(i, j, wall);
-              } else {
-                throw new Error('Invalid Encoding of Walls');
-              }
-            }
-            // Corner walls skipped
-          }
-        }
-        layout.fixCornerWalls();
-        return layout;
+  private static serializeRowWalls(elements: (SquareType | WallType)[]) {
+    const binaryList = elements.map((element) => {
+      if ('className' in element) {
+        return Serializer.wallEncodeMap.get(element.className) as number;
+      } else {
+        return 0b00;
       }
+    }).filter((x) => !!x);
+
+    const hexString = Utils.chunk(binaryList, 2)
+      .map((arr: any) => (arr[0] << 0) | (arr[1] << 2))
+      .map((num: number) => Serializer.characterMap.get(num))
+      .join('');
+
+    return hexString;
+  }
+
+  static serializeWalls(layout: Layout) {
+    const walls = layout.layout
+      .map((row, i) => {
+          return row.filter((_, j) => i % 2 === 0 || j % 2 === 0);
+      })
+      .flat();
+    return Serializer.serializeRowWalls(walls);
+  }
+
+  static *deserializeWalls(wallEncoding: string): Generator<string, null, null> {
+    const walls = wallEncoding
+      .split('')
+      .map((char) => Serializer.characterUnMap.get(char) as number)
+      .map((num) => {
+        return [
+          (num & 0b0011) >> 0,
+          (num & 0b1100) >> 2,
+        ]
+      })
+      .flat()
+      .map((wallCode) => Serializer.wallDecodeMap.get(wallCode) as string);
+
+    for (const wall of walls) {
+      yield wall;
+    }
+
+    return null;
+  }
+
+  static encodeLayoutString(layout: Layout) {
+    let layoutString = `v1 ${layout.height}x${layout.width} `;
+    for (let i = 0; i < layout.height * 2 - 1; i++) {
+      for (let j = 0; j < layout.width * 2 - 1; j++) {
+        const element = layout.layout[i][j];
+        const notCornerWall = i % 2 === 0 || j % 2 === 0;
+        const isWall = 'className' in element;
+        if (notCornerWall && !isWall) {   // Skip corner walls
+          layoutString += element.getStrRepr();
+        }
+      }
+    }
+
+    layoutString += ` ${Serializer.serializeWalls(layout)}`;
+    return encodeURI(LZString.compressToEncodedURIComponent(layoutString));
+  }
+
+  static decodeLayoutString(compressedLayoutString: string) {
+    let decompressed = LZString.decompressFromEncodedURIComponent(decodeURI(compressedLayoutString).slice(1));
+    if (decompressed === null) {
+      throw new URIError("Invalid layout string, decompression failed");
+    }
+    let [version, size, layoutString, wallString] = decompressed.split(" ");
+    if (version !== "v1") {
+      throw new URIError("Invalid layout string version");
+    }
+    let [height, width] = size.split("x").map((x) => parseInt(x));
+
+    const wallsDecoded = Serializer.deserializeWalls(wallString);
+
+    let layout = new Layout(height, width);
+    for (let i = 0; i < layout.height * 2 - 1; i++) {
+      for (let j = 0; j < layout.width * 2 - 1; j++) {
+        // Squares (2 characters + 1 for rotation)
+        if (i % 2 === 0 && j % 2 === 0) {
+          let squareStrRepr = layoutString.slice(0, 3);
+          layoutString = layoutString.slice(3);
+          layout.setElement(i, j, SquareType.fromStrRepr(squareStrRepr));
+        // Walls (0.5 characters, 1 character = 2 walls)
+        } else if (i % 2 === 0 || j % 2 === 0) { // Skip Corner Walls
+          let wallStrRepr = wallsDecoded.next().value;
+          if (!!wallStrRepr) {
+            const wall = WallType.fromStrRepr(wallStrRepr)
+            layout.setElement(i, j, wall);
+          } else {
+            throw new Error('Invalid Encoding of Walls');
+          }
+        }
+      }
+    }
+    layout.fixCornerWalls();
+    return layout;
+  }
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,16 @@
 export class Utils {
-    // https://stackoverflow.com/questions/8495687/split-array-into-chunks
-    static chunk = (arr: any[], chunkSize: number) => {
-        return arr.reduce((all, one, i) => {
-            const ch = Math.floor(i / chunkSize);
-            all[ch] = [].concat((all[ch] || []),one);
-            return all;
-         }, [])
-    }
+  // https://stackoverflow.com/questions/8495687/split-array-into-chunks
+  static chunk = (arr: any[], chunkSize: number) => {
+    return arr.reduce((all, one, i) => {
+      const ch = Math.floor(i / chunkSize);
+      all[ch] = [].concat((all[ch] || []),one);
+      return all;
+      }, [])
+  }
 
-    static chunkJoin = (arr: any[], chunkSize: number) => {
-        const chunks = Utils.chunk(arr, chunkSize)
-            .map((x: any) => x.join(''));
-        return chunks;
-    }
+  static chunkJoin = (arr: any[], chunkSize: number) => {
+    const chunks = Utils.chunk(arr, chunkSize)
+      .map((x: any) => x.join(''));
+    return chunks;
+  }
 }
